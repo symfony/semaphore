f7dbcd70babaf58846f25630f230a8dc19ef5075
Nicolas Grekas <nicolas.grekas@gmail.com>
2023-02-07 10:31:13 +0100

    minor #49253 [PHPUnit 10] Use `TestCase` suffix for abstract tests in `/Tests/` (OskarStark)
    This PR was merged into the 6.3 branch.
    
    Discussion
    ----------
    
    [PHPUnit 10] Use `TestCase` suffix for abstract tests in `/Tests/`
    
    | Q             | A
    | ------------- | ---
    | Branch?       | 6.3
    | Bug fix?      | no
    | New feature?  | no
    | Deprecations? | no
    | Tickets       | Refs https://github.com/symfony/symfony/pull/49233
    | License       | MIT
    | Doc PR        | n/a
    
    Replaces #49234
    
    Using `Test` suffix is deprecated since PHPUnit 10
    
    Spotted in
    * https://github.com/symfony/symfony/pull/49233
    
    Commits
    -------
    
    cb3db968e4 [PHPUnit 10] Use `TestCase` suffix for abstract tests in `/Tests/`

--- Tests/Store/AbstractRedisStoreTestCase.php
+++ Tests/Store/AbstractRedisStoreTestCase.php
@@ -17,7 +17,7 @@ use Symfony\Component\Semaphore\Store\RedisStore;
 /**
  * @author Jérémy Derussé <jeremy@derusse.com>
  */
-abstract class AbstractRedisStoreTest extends AbstractStoreTest
+abstract class AbstractRedisStoreTestCase extends AbstractStoreTestCase
 {
     /**
      * Return a RedisConnection.
